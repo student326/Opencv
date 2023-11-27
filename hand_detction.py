ea1572b68de93a2ae6a21feef3820ebeded5181a
Awais PAK <67801369+student326@users.noreply.github.com>
2023-11-27 23:11:59 +0500

    Add files via upload
    I have made a hand detection program which detect your hands.

---
+++ hand_detction.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import time 
+
+cap = cv2.VideoCapture(1)
+
+
+mp_Hands = mp.solutions.hands
+hands = mp_Hands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
+ptime = 0 
+cTime = 0
+while True:
+    success, img = cap.read()
+
+    img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_RGB)
+    #print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for hand_Lms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, hand_Lms, mp_Hands.HAND_CONNECTIONS) 
+
+
+    cTime = time.time()
+    fps = 1/ (cTime - ptime)
+
+    ptime = cTime
+
+    cv2.putText(img , str(int(fps)), (10 , 70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255),3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
